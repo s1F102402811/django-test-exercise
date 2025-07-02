5d74ef55c369aa6c71a55f3c4517995056958d1c
1f10240281 <s1f102402811@iniad.org>
2025-07-02 23:25:38 +0900

    テストの追加2

--- todo/tests.py
+++ todo/tests.py
@@ -54,9 +54,17 @@ class TaskModelTestCase(TestCase):
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
-        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.get('/')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 0)
+
+    def test_index_post(self):
+        client = Client()
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
+        response = client.post('/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.tenplates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context['tasks']), 1)
